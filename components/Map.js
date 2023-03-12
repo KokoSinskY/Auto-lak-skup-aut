eb468ed33106999ef88abd963b10761d42d8a5ec
Artur <arturnawrocki121@gmail.com>
2023-03-12 11:47:28 +0100

    done with google map API

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,22 @@
-import Box from "@mui/material/Box";
+import { useState } from "react";
 import Grid from "@mui/material/Grid";
-
-import { GoogleMap, useJsApiLoader, MarkerF, InfoWindow } from "@react-google-maps/api";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { GoogleMap, useJsApiLoader, MarkerF, InfoWindowF } from "@react-google-maps/api";
 
 const Map = () => {
-  const mapLocation = { lat: 51.796242, lng: 19.430085 };
-  const infoWindowLocation = { lat: 51.796736, lng: 19.430095 };
-
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: "AIzaSyBE-tgBICt919T6lwteqv8CF9L1Q8tnA6Q",
   });
+
+  const mapLocation = { lat: 51.796242, lng: 19.430085 };
+  const infoWindowLocation = { lat: 51.796736, lng: 19.430095 };
+
+  const [showingInfoWindow, setShowingInfoWindow] = useState(false);
+
+  const onMarkerClick = () => {
+    setShowingInfoWindow(true);
+  };
 
   const divStyle = {
     background: `white`,
@@ -18,13 +25,28 @@ const Map = () => {
   if (!isLoaded) return <div>Loading...</div>;
 
   return (
-    <Box>
-      <Grid>
+    <Grid>
+      <Grid container item xl={6} lg={6} md={6} sm={12}>
         <GoogleMap zoom={15.5} center={mapLocation} mapContainerClassName="map-container">
-          <MarkerF position={mapLocation} />
+          <MarkerF onClick={onMarkerClick} position={mapLocation} />
+          {!showingInfoWindow ? null : (
+            <InfoWindowF
+              position={infoWindowLocation}
+              visible={showingInfoWindow}
+              onCloseClick={() => setShowingInfoWindow(false)}
+            >
+              <Paper>
+                <Typography variant="headline" component="h2">
+                  Auto-Lak
+                </Typography>
+                <Typography component="div">ul. Grunwaldzka 33a</Typography>
+                <Typography component="div">793-308-669</Typography>
+              </Paper>
+            </InfoWindowF>
+          )}
         </GoogleMap>
       </Grid>
-    </Box>
+    </Grid>
   );
 };
 
