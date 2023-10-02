c96a81ab4471183dc4af5a311fc31d4de187d4d2
Artur <arturnawrocki121@gmail.com>
2023-10-02 19:22:37 +0200

    hotfix

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    return config;
-  },
 };
 
 module.exports = nextConfig;
